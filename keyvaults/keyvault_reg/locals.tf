9588092d919cf8acf4eb072743856b08f99abbb4
ak1922 <aoakyaw@protonmail.com>
2024-06-09 12:38:34 -0400

    Locals block for project tags.

---
+++ keyvaults/keyvault_reg/locals.tf
@@ -0,0 +1,10 @@
+# Block to hold project tags
+locals {
+  project_tags = {
+    cost_criteria = "intermetiate"
+    managed_by    = "Terraform"
+    gitrepo       = "tf_ak_azure_pub"
+    project       = "keyvaults"
+    sub_project   = "keyvault_reg"
+  }
+}
