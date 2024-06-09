ae67b55036a2207a4df8314c612127d2f3c1b312
ak1922 <aoakyaw@protonmail.com>
2024-06-09 14:58:07 -0400

    Data source for subnet

--- keyvaults/keyvault_reg/data.tf
+++ keyvaults/keyvault_reg/data.tf
@@ -5,3 +5,10 @@ data "azurerm_client_config" "current" {}
 data "azurerm_resource_group" "resource_group" {
   name = "tfakazurepub-rg"
 }
+
+# Data source service subnet.
+data "azurerm_subnet" "servicesub" {
+  name                 = "atlas_vnet_service_subnet"
+  resource_group_name  = data.azurerm_resource_group.resource_group.name
+  virtual_network_name = "data.azurerm_virtual_network.vnet.name"
+}
