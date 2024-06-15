e98d940e717a1a340f5625993803aa7b363f53c8
ak1922 <aoakyaw@protonmail.com>
2024-06-15 16:15:23 -0400

    Locals fmt.

--- storageaccount/storage_account_reg/locals.tf
+++ storageaccount/storage_account_reg/locals.tf
@@ -1,9 +1,9 @@
 locals {
   akstorage_tags = {
     cost_criteria = "intermediate"
-    managed_by  = "Terraform"
-    gitrepo     = "tf_ak_azure_pub"
-    project     = "storageaccount"
-    sub_project = "storage_account_reg"
+    managed_by    = "Terraform"
+    gitrepo       = "tf_ak_azure_pub"
+    project       = "storageaccount"
+    sub_project   = "storage_account_reg"
   }
 }
