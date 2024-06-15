4e362a3e7a988250338ece57375b7caf71143ebd
ak1922 <aoakyaw@protonmail.com>
2024-06-15 16:14:52 -0400

    Data source key vault.

--- storageaccount/storage_account_reg/data.tf
+++ storageaccount/storage_account_reg/data.tf
@@ -1,4 +1,10 @@
 # Data source resource group.
 data "azurerm_resource_group" "resource_group" {
-  name = "aoa_rg"
+  name = "tfakazurepub-rg"
+}
+
+# Data source Key Vault.
+data "azurerm_key_vault" "keyvault" {
+  name                = "tfakazurepub-kv"
+  resource_group_name = data.azurerm_resource_group.resource_group.name
 }
