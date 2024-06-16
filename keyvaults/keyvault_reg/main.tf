ba73a87319ed5749e17d18f4df578130a19bb0ab
ak1922 <aoakyaw@protonmail.com>
2024-06-16 19:24:50 -0400

    Key vault complete.

--- keyvaults/keyvault_reg/main.tf
+++ keyvaults/keyvault_reg/main.tf
@@ -43,3 +43,45 @@ resource "azurerm_monitor_diagnostic_setting" "keyvault_diagsetting" {
 
   storage_account_id = data.azurerm_storage_account.storage.id
 }
+
+# Private endpoint for Key Vault.
+resource "azurerm_private_endpoint" "keyvault_endpoint" {
+  name                = local.module_args.endpoint_name
+  location            = var.location
+  resource_group_name = element(var.rgnames, 1)
+  subnet_id           = data.azurerm_subnet.servicesub.id
+
+  private_dns_zone_group {
+    name                 = join("", [replace(local.module_args.vault_name, "-kv", ""), "-kvpdnszg"])
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.keyvault_dnszone.id]
+  }
+
+  private_service_connection {
+    name                           = join("", [replace(local.module_args.vault_name, "-kv", ""), "-pricon"])
+    is_manual_connection           = var.is_manual_connection
+    subresource_names              = var.subresource_names
+    private_connection_resource_id = azurerm_key_vault.keyvault.id
+  }
+
+  tags = local.project_tags
+}
+
+# Alert rules for Key Vault.
+resource "azurerm_monitor_activity_log_alert" "alert_rule" {
+  for_each = var.activity_log
+
+  name                = each.value.name
+  description         = each.value.description
+  resource_group_name = element(var.rgnames, 1)
+  scopes              = [azurerm_key_vault.keyvault.id]
+
+  criteria {
+    category       = var.category
+    statuses       = each.value.criteria.statuses
+    operation_name = each.value.criteria.operation_name
+  }
+
+  action {
+    action_group_id = data.azurerm_monitor_action_group.actiongroup.id
+  }
+}
