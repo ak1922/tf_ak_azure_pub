5c8ba8b66c319739069a0b2e02b716f8cca9f7b2
ak1922 <aoakyaw@protonmail.com>
2024-06-16 12:35:57 -0400

    Locals for project.

--- storageaccount/storage_account_reg/locals.tf
+++ storageaccount/storage_account_reg/locals.tf
@@ -1,9 +1,11 @@
 locals {
-  akstorage_tags = {
+  project_tags = {
     cost_criteria = "intermediate"
     managed_by    = "Terraform"
     gitrepo       = "tf_ak_azure_pub"
     project       = "storageaccount"
     sub_project   = "storage_account_reg"
   }
+
+  storage_name = join("", [replace(local.project_tags.gitrepo, "_", ""), random_integer.number.result])
 }
