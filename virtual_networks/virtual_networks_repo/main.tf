2f1b4d3bd4aa50f634deff154474a62b02a5208a
ak1922 <aoakyaw@protonmail.com>
2024-06-18 14:40:27 -0400

    New subnet for posygres.

--- virtual_networks/virtual_networks_repo/main.tf
+++ virtual_networks/virtual_networks_repo/main.tf
@@ -18,4 +18,30 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = data.azurerm_resource_group.resource_group.name
 
   private_endpoint_network_policies_enabled = each.value.private_endpoint_network_policies_enabled
+
+  delegation {
+    name = each.value.name
+    service_delegation {
+      name    = each.value.name
+      actions = each.value["actions"]
+    }
+  }
 }
+
+# resource "azurerm_subnet" "postgres_subnet" {
+#   name                 = "tfakazurepub-vnet-ppsqldb-sub"
+#   address_prefixes     = ["192.168.0.16/29"]
+#   virtual_network_name = azurerm_virtual_network.virtual_network.name
+#   resource_group_name  = data.azurerm_resource_group.resource_group.name
+
+#   delegation {
+#     name = "pgfs"
+
+#     service_delegation {
+#       name = "Microsoft.DBforPostgreSQL/flexibleServers"
+#       actions = [
+#         "Microsoft.Network/virtualNetworks/subnets/join/action"
+#       ]
+#     }
+#   }
+# }
