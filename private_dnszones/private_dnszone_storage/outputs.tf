863ea68f2afd8befb822342257f2c059eedb2853
ak1922 <aoakyaw@protonmail.com>
2024-06-18 15:17:52 -0400

    Added description.

--- private_dnszones/private_dnszone_storage/outputs.tf
+++ private_dnszones/private_dnszone_storage/outputs.tf
@@ -5,4 +5,5 @@ output "storage_dnszone_id" {
 
 output "alert_rule_ids" {
   value = values(azurerm_monitor_activity_log_alert.alert_rule).*.id
+  description = "The IDs of activity log alert rules"
 }
