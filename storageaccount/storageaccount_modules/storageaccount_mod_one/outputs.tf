18077bcd39d7cc0d5d1fbf20e0250d9336017f0d
ak1922 <aoakyaw@protonmail.com>
2024-06-21 16:54:49 -0400

    Blocks for networking.

--- storageaccount/storageaccount_modules/storageaccount_mod_one/outputs.tf
+++ storageaccount/storageaccount_modules/storageaccount_mod_one/outputs.tf
@@ -1,7 +1,15 @@
 output "user_identity_id" {
-    value = module.identity.user_identity_id
+  value = module.identity.user_identity_id
 }
 
 output "user_identity_role_id" {
-    value = module.identity.user_identity_role_id
+  value = module.identity.user_identity_role_id
+}
+
+output "private_dnszone_name" {
+  value = module.networking.private_dnszone_name
+}
+
+output "private_dnszone_id" {
+  value = module.networking.private_dnszone_id
 }
