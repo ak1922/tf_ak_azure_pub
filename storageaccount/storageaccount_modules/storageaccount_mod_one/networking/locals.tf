a263ea06d03faec864e9e5b8af69e83697a237f5
ak1922 <aoakyaw@protonmail.com>
2024-06-22 14:05:41 -0400

    Locals with arguments.

--- storageaccount/storageaccount_modules/storageaccount_mod_one/networking/locals.tf
+++ storageaccount/storageaccount_modules/storageaccount_mod_one/networking/locals.tf
@@ -9,10 +9,22 @@ locals {
     sub_project_addon = "storageaccount_modules_one"
     environment       = "dev"
   }
+
+  common_name = join("", [local.module_tags.project, var.app_name])
 }
 
+# Locals block for dns zone arguments.
 locals {
   dnszone_args = {
-    link_name = join("-", [local.module_tags.project, "pdnsz", ".com"])
+    link_name = join("", [local.module_tags.project, "-pdnsz", ".com"])
+  }
+}
+
+# Locals block for private enapoint arguments.
+locals {
+  endpoint_args = {
+    endpoint_name = join("", [local.common_name, "-pep"])
+    group_name    = join("", [local.common_name, "-pdnszg"])
+    private_name  = join("", [local.common_name, "-priconn"])
   }
 }
