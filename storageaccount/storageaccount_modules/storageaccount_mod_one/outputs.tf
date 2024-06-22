4e73cc1cea530e57ea82812c1b343b2cee94dcee
ak1922 <aoakyaw@protonmail.com>
2024-06-22 14:01:55 -0400

    Blocks for project.

--- storageaccount/storageaccount_modules/storageaccount_mod_one/outputs.tf
+++ storageaccount/storageaccount_modules/storageaccount_mod_one/outputs.tf
@@ -13,3 +13,11 @@ output "private_dnszone_name" {
 output "private_dnszone_id" {
   value = module.networking.private_dnszone_id
 }
+
+output "storage_id" {
+  value = module.storage.storage_id
+}
+
+output "storage_endpoint_id" {
+  value = module.networking.storage_endpoint_id
+}
