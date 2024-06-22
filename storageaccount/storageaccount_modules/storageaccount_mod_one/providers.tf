0c384c2d506e181d31cccf0f23dca9e32883cd4d
ak1922 <aoakyaw@protonmail.com>
2024-06-22 14:01:28 -0400

    Modules complete.

--- storageaccount/storageaccount_modules/storageaccount_mod_one/providers.tf
+++ storageaccount/storageaccount_modules/storageaccount_mod_one/providers.tf
@@ -26,9 +26,29 @@ module "identity" {
   role          = "Key Vault Crypto Service Encryption User"
 }
 
+module "storage" {
+  depends_on                 = [module.identity]
+  source                     = "./storage"
+  app_name                   = "oaks"
+  assigned_id                = module.identity.user_identity_id
+  identity_ids               = [module.identity.user_identity_id]
+  key_size                   = 2048
+  key_type                   = "RSA"
+  keyvault_name              = "tfakazurepub-kv"
+  preferred_container_delete = null
+  preferred_delete_retention = null
+  preferred_key_expiration   = null
+  preferred_keyname          = null
+  preferred_restore_policy   = null
+}
+
 module "networking" {
   source     = "./networking"
-  depends_on = [module.identity]
+  depends_on = [module.storage]
 
-  dnszone_name = "privatelink.blob.core.windows.net"
+  dnszone_name         = "privatelink.blob.core.windows.net"
+  app_name             = "oaks"
+  connect_resource     = module.storage.storage_id
+  is_manual_connection = false
+  subresource_names    = ["blob"]
 }
