da46276edb74e05204f2693420c76fd020d519e3
ak1922 <aoakyaw@protonmail.com>
2024-06-23 14:14:55 -0400

    Changes to subnets.

--- virtual_networks/virtual_networks_repo/main.tf
+++ virtual_networks/virtual_networks_repo/main.tf
@@ -13,35 +13,19 @@ resource "azurerm_subnet" "subnet" {
 
   name                 = each.value.name
   address_prefixes     = each.value.address_prefixes
-  service_endpoints    = each.value.service_endpoints
-  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  service_endpoints    = lookup(each.value, "service_endpoints", null)
   resource_group_name  = data.azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
 
-  private_endpoint_network_policies_enabled = each.value.private_endpoint_network_policies_enabled
+  dynamic "delegation" {
+    for_each = lookup(each.value, "delegation", null) != null ? [each.value.delegation] : []
 
-  delegation {
-    name = each.value.name
-    service_delegation {
-      name    = each.value.name
-      actions = each.value["actions"]
+    content {
+      name = delegation.value.name
+      service_delegation {
+        name    = delegation.value.service_delegation.name
+        actions = delegation.value.service_delegation.actions
+      }
     }
   }
 }
-
-# resource "azurerm_subnet" "postgres_subnet" {
-#   name                 = "tfakazurepub-vnet-ppsqldb-sub"
-#   address_prefixes     = ["192.168.0.16/29"]
-#   virtual_network_name = azurerm_virtual_network.virtual_network.name
-#   resource_group_name  = data.azurerm_resource_group.resource_group.name
-
-#   delegation {
-#     name = "pgfs"
-
-#     service_delegation {
-#       name = "Microsoft.DBforPostgreSQL/flexibleServers"
-#       actions = [
-#         "Microsoft.Network/virtualNetworks/subnets/join/action"
-#       ]
-#     }
-#   }
-# }
