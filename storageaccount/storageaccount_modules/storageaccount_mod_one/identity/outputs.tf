1d40f50b652c80f4ccb64506cc264bc9eb624f78
NEO\oracle <something@you.com>
2024-11-12 10:42:00 -0500

    New folder

--- storageaccount/storageaccount_modules/storageaccount_mod_one/identity/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "user_identity_id" {
-  value = azurerm_user_assigned_identity.user_identity.id
-}
-
-output "user_identity_role_id" {
-  value = azurerm_role_assignment.user_identity_role.id
-}
